ed44405a0e2d659789283628d2fdb5e5eee465bd
Shariar Islam <shariarn85@gmail.com>
2024-01-14 00:55:43 +0600

    contact and about page added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,17 +1,25 @@
-import PropTypes from 'prop-types'
- 
-const About = ({props}) => {
- return (
-    <div> 
-         I am available from About 
+import aboutImage from "../../assets/about.jpg";
+const About = () => {
+  return (
+    <div className=" flex flex-col md:flex-row">
+      <div className=" flex-1 p-2">
+        <img className=" rounded-lg" src={aboutImage} alt="" />
+      </div>
+      <div className=" flex-1 p-2 text-center my-auto">
+        <h2 className=" font-bold text-3xl mb-5">Our Journey</h2>
+        <p className=" text-xl ">
+          Explore the winding path that led us to
+          <span className=" bg-[#33547D] text-white p-1">
+            Graydot Technologies
+          </span>
+          . From late-night brainstorming sessions to overcoming challenges, our
+          story is one of passion and perseverance.
+        </p>
+      </div>
     </div>
- )
-}
- 
-export default About
- 
- 
-About.propTypes = {
- 
- props: PropTypes.any, 
+  );
 };
+
+export default About;
+
+About.propTypes = {};
