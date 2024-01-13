322aa7c72d1a377de0400c10a3299715d180f96a
Shariar Islam <shariarn85@gmail.com>
2024-01-14 01:02:01 +0600

    about and contact page information added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,23 +1,30 @@
-
- import contactImage from '../../assets/contact.jpg'
+import contactImage from "../../assets/contact.jpg";
 const Contact = () => {
- return (
-    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://raw.githubusercontent.com/ShariarNiaj05/Graydot-Technologies-Task-Client/main/src/assets/contact.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Contact With Us</h1>
+          <p className="mb-5">
+            Have a question, suggestion, or just want to say hello? We'd love to
+            hear from you. Feel free to reach out using the contact information
+            below or by filling out the form.
+          </p>
+          <p>Email: shariarn85@gmail.com </p>
+          <p>Phone: +8801850893033</p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
- )
-}
- 
-export default Contact
- 
- 
-Contact.propTypes = {
- 
+  );
 };
+
+export default Contact;
+
+Contact.propTypes = {};
